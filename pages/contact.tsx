522f02959185b7cffa8abe4fdc1b3370c53226b7
Dhruv <heisdhruv7@gmail.com>
2025-10-23 03:06:15 +0530

    Dynamically import FormSection for client-side rendering

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 import Page from "components/Page";
 import { media } from "utils/media";
-import FormSection from "views/ContactPage/FormSection";
+import dynamic from "next/dynamic";
 import InformationSection from "views/ContactPage/InformationSection";
-import MapSection from "views/ContactPage/MapSection";
-import Divider from "components/Divider";
 import { PaymentBox, PaymentContainer } from ".";
 import TextBubble from "@/components/TextBubble";
 import ServiceCTA from "@/views/HomePage/ServiceCTA";
 import { useRouter } from "next/router";
+
+// Client-only FormSection
+const FormSection = dynamic(() => import("views/ContactPage/FormSection"), {
+  ssr: false,
+});
 
 export default function ContactPage() {
   const router = useRouter();
@@ -30,9 +33,7 @@ export default function ContactPage() {
       <div className="flex flex-col justify-center space-y-10">
         <ServiceCTA />
         <button
-          onClick={() => {
-            router.push("/coupons");
-          }}
+          onClick={() => router.push("/coupons")}
           className="bg-[#751318] text-2xl px-32 py-2 text-white mx-auto"
         >
           FOR COUPONS CLICK HERE
