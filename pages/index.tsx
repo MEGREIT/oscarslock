730cba3f092b6af616506454c079087ea6e75970
Dhruv <heisdhruv7@gmail.com>
2025-10-23 04:34:40 +0530

    Refactor index.tsx to remove unused code
    Removed unused imports and commented code, updated alt attributes for images, and cleaned up the component structure.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,27 @@
-import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import { InferGetServerSidePropsType } from "next";
 import Head from "next/head";
 import styled from "styled-components";
-import BasicSection from "components/BasicSection";
-// import Link from "components/Link";
 import { EnvVars } from "env";
 import Cta from "views/HomePage/Cta";
-import Partners from "views/HomePage/Partners";
 import Divider from "components/Divider";
 import Slider from "components/Slider";
-import BlogPostSlider from "@/views/HomePage/BlogPostSlider";
 import "./style.css";
 import {
-  getAllPartners,
   getAllPosts,
   getAllServices,
   getAllTestimonials,
   getClient,
 } from "@/sanity/lib/client";
-import HomeBasicSection from "@/components/HomeBasicSection";
-import ServiceCard from "@/components/ServiceCard";
-import ServicesSection from "@/components/ServicesSection";
-import OurTeam from "@/views/AboutPage/OurTeam";
-import Tabs from "@/components/Tabs";
-import TextWrapper from "@/components/TextWrapper";
-import { PortableText } from "@portabletext/react";
-import { description } from "../lib/demo.data";
-import PostBody from "@/components/PostBody";
 import { useRouter } from "next/router";
 import About from "@/components/About";
 import { media } from "@/utils/media";
-import citiesData from "@/utils/cities.json";
 import ServicesGrid from "@/components/ServicesGrid";
 import TextBubble from "@/components/TextBubble";
 import PhotoSlider from "@/components/PhotoSlider";
 import { useEffect } from "react";
-import { calculateDistance } from "./_app";
 import GoogleScript from "@/components/Script";
 import PhoneBtn from "@/components/PhoneBtn";
+import OurTeam from "@/views/AboutPage/OurTeam";
 
 const client = getClient();
 
@@ -57,9 +42,9 @@ export default function Homepage({
   testimonials,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
-  // console.log(services);
 
   useEffect(() => {}, []);
+  
   return (
     <>
       <Head>
@@ -67,17 +52,6 @@ export default function Homepage({
         <meta
           name="description"
           content="Reliable locksmith service offering residential, commercial, and automotive lock solutions. Get fast, secure assistance for emergencies, lock changes, key replacements, and high-security systems. Licensed, bonded, and insured professionals serving your security needs."
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WPNF8ZTD');
-              `,
-          }}
         />
       </Head>
       <GoogleScript />
@@ -93,11 +67,11 @@ export default function Homepage({
               </div>
               <PaymentBox>
                 <PaymentContainer>
-                  <img src="/payment.png" />
+                  <img src="/payment.png" alt="Payment methods accepted" />
                 </PaymentContainer>
                 <PhoneBtn phone="(508) 736-7178" />
                 <TextBubble />
-                <img src="/logos/oscar-logo.png" className="w-[25rem] ml-0" />
+                <img src="/logos/oscar-logo.png" className="w-[25rem] ml-0" alt="Oscar's Lock logo" />
               </PaymentBox>
             </div>
             <MapContainer>
@@ -105,9 +79,6 @@ export default function Homepage({
               <PhoneBtn phone="(508) 736-7178" />
               <PhotoSlider />
             </MapContainer>
-            {/* <MapContainer>
-              
-            </MapContainer> */}
           </div>
         </WhiteBackgroundContainer>
         <DarkerBackgroundContainer>
@@ -136,13 +107,10 @@ const HomepageWrapper = styled.div`
 `;
 
 export const PaymentContainer = styled.div`
-  /* width: 100%; */
   display: flex;
   justify-content: start;
   margin-top: -3.5rem;
-  /* margin: 2rem 0; */
   align-items: start;
-  /* border-left: solid 1px #83838390; */
 
   img {
     margin-bottom: auto;
@@ -152,6 +120,7 @@ export const PaymentContainer = styled.div`
     margin-top: 0rem;
   }
 `;
+
 export const PaymentBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -199,7 +168,6 @@ export const WhiteBackgroundContainer = styled.div`
   }
   ${media(">largeDesktop")} {
     align-items: center;
-    /* max-width: 60vw; */
     margin: 0 auto;
   }
   @media (min-width: 375px) and (max-width: 640px) {
@@ -218,7 +186,6 @@ export const WhiteBackgroundContainer = styled.div`
 export const MapContainer = styled.div`
   display: flex;
   flex-direction: row;
-  /* background-color: yellow; */
   max-width: 60vw;
   align-items: center;
   padding: 0 0rem;
@@ -238,7 +205,6 @@ export const MapContainer = styled.div`
 
   @media (min-width: 1280px) {
     max-width: 1190px;
-    /* margin: 0 2rem; */
     overflow: hidden;
   }
   @media (min-width: 1280px) and (max-width: 2652px) {
@@ -247,9 +213,6 @@ export const MapContainer = styled.div`
   }
   @media (max-width: 1440px) and (min-width: 768px) {
     width: 100%;
-    /* width: 90vw; */
-    /* padding-left: 5rem; */
-    /* margin: 0 auto; */
   }
 `;
 
