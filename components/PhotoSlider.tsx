66407effc8dbc792021f09b8857cfcea27ba8980
Dhruv <heisdhruv7@gmail.com>
2025-10-23 05:12:07 +0530

    Refactor PhotoSlider component for dynamic imports

--- components/PhotoSlider.tsx
+++ components/PhotoSlider.tsx
@@ -1,23 +1,28 @@
 import React from "react";
+import dynamic from "next/dynamic";
+
+// Import Swiper dynamically with SSR disabled
+const Swiper = dynamic(
+  () => import("swiper/react").then((mod) => mod.Swiper),
+  { ssr: false }
+);
+
+const SwiperSlide = dynamic(
+  () => import("swiper/react").then((mod) => mod.SwiperSlide),
+  { ssr: false }
+);
+
+// Import Swiper modules - these don't need dynamic import
 import { A11y, Autoplay, Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 
 const images = [
   "/slider/1.jpg",
-  // "/slider/2.jpg",
   "/slider/3.jpg",
-  // "/slider/4.jpg",
-  // "/slider/5.jpg",
-  // "/slider/6.jpg",
-  // "/slider/7.jpg",
-  // "/slider/8.jpg",
   "/slider/9.jpg",
   "/slider/10.jpg",
   "/slider/11.jpg",
-  // "/slider/12.jpg",
   "/slider/13.jpg",
   "/slider/14.jpeg",
-  // "/slider/15.jpeg",
   "/slider/16.jpeg",
   "/slider/17.jpeg",
   "/slider/18.jpeg",
@@ -26,18 +31,9 @@ const images = [
 ];
 
 const PhotoSlider: React.FC = () => {
-  const location = {
-    latitude: 42.2898, // Replace with any random latitude in Georgia
-    longitude: -71.6802, // Replace with any random longitude in Georgia
-  };
-  const APIKey = `AIzaSyCiHbmEkwBoB-oyCjS7QQVT7VqpyVYn3Jk`;
-  const mapUrl = `https://www.google.com/maps/embed/v1/view?zoom=8&center=${location.latitude},${location.longitude}&key=${APIKey}`;
-
   return (
-    <div className=" flex  w-[80vw] md:w-[267px] h-auto flex-col">
-      {/* <h1 className="text-left text-[6rem] ">Come Find us</h1> */}
-      <div className="w-full border-2 border-gray-500 p-0  h-[30vh] min-h-[260px] rounded-lg overflow-hidden shadow-lg">
-        {/* picture slider */}
+    <div className="flex w-[80vw] md:w-[267px] h-auto flex-col">
+      <div className="w-full border-2 border-gray-500 p-0 h-[30vh] min-h-[260px] rounded-lg overflow-hidden shadow-lg">
         <Swiper
           modules={[Navigation, Autoplay, A11y]}
           slidesPerView={1}
@@ -49,12 +45,12 @@ const PhotoSlider: React.FC = () => {
           navigation={false}
           loop
         >
-          {images.map((image: any, idx: any) => (
+          {images.map((image: string, idx: number) => (
             <SwiperSlide key={idx}>
               <img
                 className="h-[30vh] min-h-[260px] w-full object-cover"
                 src={image}
-                alt=""
+                alt={`Slider image ${idx + 1}`}
               />
             </SwiperSlide>
           ))}
