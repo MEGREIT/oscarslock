bb8bba9c07cb7cbbd5193a2be09a73fd6dfc2b95
Dhruv <heisdhruv7@gmail.com>
2025-12-30 19:26:04 +0530

    Update Cta.tsx

--- views/HomePage/Cta.tsx
+++ views/HomePage/Cta.tsx
@@ -1,11 +1,5 @@
-import NextLink from "next/link";
 import React from "react";
 import styled from "styled-components";
-import Button from "components/Button";
-import ButtonGroup from "components/ButtonGroup";
-// import Container from 'components/Container';
-import OverTitle from "components/OverTitle";
-// import SectionTitle from 'components/SectionTitle';
 import { media } from "utils/media";
 
 export default function Cta() {
@@ -13,118 +7,51 @@ export default function Cta() {
     <CtaWrapper>
       <Container>
         <Stack>
-          {/* <OverTitle>Lorem ipsum dolor sit amet</OverTitle> */}
-          
-          {/* --- TEXT REMOVED FROM HERE --- */}
-          {/* <SectionTitle>
-            Don't Wait, Reach Out To Oscars Lock & Key Services!
-          </SectionTitle> */}
-          {/* ----------------------------- */}
-
-          {/* <Description> Contact Us Today to Get Started!</Description> */}
+          {/* Main Heading ONLY - Subtext removed to avoid duplication */}
+          <MainHeading>
+            Take a Look At Our Coupons - You Might Qualify For a Discount!
+          </MainHeading>
         </Stack>
       </Container>
     </CtaWrapper>
   );
 }
 
-const Description = styled.div`
-  font-size: 1.8rem;
-  color: #15233e;
-`;
+// --- STYLES ---
 
-const SectionTitle = styled.h1`
-  font-size: 20px;
-  font-weight: bold;
-  max-width: 50%;
+const MainHeading = styled.h2`
+  font-family: "Times New Roman", serif;
+  font-size: 3.2rem;
+  font-weight: 700;
   text-align: center;
-  color: #15233e;
-  background-color: white;
+  color: #0A3161; /* Dark Blue */
+  margin-bottom: 0; 
+  line-height: 1.2;
 
-  ${media("<=desktop")} {
-  }
-  ${media("<tablet")} {
-    /* font-size: 3rem; */
-    max-width: 90%;
+  ${media("<=tablet")} {
+    font-size: 2.4rem;
+    padding: 0 1rem;
   }
 `;
 
 const Container = styled.div`
-  /* background: rgb(21, 35, 62, 0.8); */
   width: 100%;
-  height: 100%;
   margin: 0 auto;
   padding: 0 2rem;
-`;
-
-const Link = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 250px;
-  height: 50px;
-  line-height: 50px;
-  font-weight: bold;
-  text-decoration: none;
-  font-size: 1.2rem;
-  background: transparent;
-  text-align: center;
-  color: #fff;
-  text-transform: uppercase;
-  letter-spacing: 4px;
-  border: 3px solid rgb(255, 255, 255);
-  transition: all 0.35s;
-  border-radius: 1rem;
-  cursor: pointer;
-
-  &:hover {
-    width: 200px;
-    letter-spacing: 2px;
-    border: 3px solid rgb(255, 175, 1);
-    background: rgb(255, 175, 1);
-    color: rgb(255, 255, 255);
-  }
 `;
 
 const Stack = styled.div`
   display: flex;
   flex-direction: column;
-  color: #15233e;
-  text-align: center;
   align-items: center;
   justify-content: center;
-  margin-bottom: 1rem;
-
-  & > *:not(:first-child) {
-    max-width: 80%;
-  }
-
-  ${media("<=tablet")} {
-    text-align: center;
-
-    & > *:not(:first-child) {
-      max-width: 100%;
-    }
-  }
 `;
 
-// const OutlinedButton = styled(Button)`
-//   border: 1px solid rgb(255,255,255);
-//   color: rgb(255,255,255);
-// `;
-
 const CtaWrapper = styled.div`
-  /* background: url("/1.jpg") no-repeat center center; */
-  background-color: white;
-  background-size: cover; /* Ensures the background image covers the entire container */
-  /* height: 50vh; */
+  background-color: transparent;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 4rem;
   overflow: hidden;
-
-  /* to change later */
-  ${media("<=tablet")} {
-  }
 `;
