9f6d47ff8d1b5caecebda7b838c841bcfaf1f9ec
Dhruv <heisdhruv7@gmail.com>
2026-01-03 04:59:49 +0530

    Update terms-conditions.tsx

--- pages/terms-conditions.tsx
+++ pages/terms-conditions.tsx
@@ -335,7 +335,7 @@ const LogoImage = styled.div`
 
 const PageTitle = styled.h1`
   font-family: "Times New Roman", serif;
-  font-size: 3rem;
+  font-size: 2rem;
   color: #0A3161;
   font-weight: 700;
   text-align: center;
@@ -343,13 +343,13 @@ const PageTitle = styled.h1`
   line-height: 1.1;
   
   ${media("<=tablet")} {
-    font-size: 2.4rem;
+    font-size: 1.6rem;
   }
 `;
 
 const PageSubTitle = styled.h2`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.8rem;
+  font-size: 1.4rem;
   color: #1e4d8b;
   font-weight: 700;
   text-align: center;
@@ -358,13 +358,13 @@ const PageSubTitle = styled.h2`
   line-height: 1.3;
   
   ${media("<=tablet")} {
-    font-size: 1.6rem;
+    font-size: 1.2rem;
   }
 `;
 
 const TextContent = styled.div`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.4rem;
+  font-size: 1.1rem;
   line-height: 1.6;
   color: #1e4d8b;
 
@@ -404,13 +404,13 @@ const TextContent = styled.div`
   }
 
   ${media("<=tablet")} {
-    font-size: 1.3rem;
+    font-size: 0.9rem;
   }
 `;
 
 const SectionHeader = styled.h3`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.6rem;
+  font-size: 1.3rem;
   color: #1e4d8b;
   font-weight: 700;
   margin-top: 1.5rem;
@@ -420,7 +420,7 @@ const SectionHeader = styled.h3`
   padding-bottom: 10px;
 
   ${media("<=tablet")} {
-    font-size: 1.4rem;
+    font-size: 1.1rem;
   }
 `;
 
