922eabbfbc5c01ac13fba933c441246e55d94647
Dhruv <heisdhruv7@gmail.com>
2026-01-04 06:59:48 +0530

    Update privacy-policy.tsx

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -76,7 +76,7 @@ export default function PrivacyPolicy() {
       <MainWrapper>
         <ContentRow>
           
-          {/* LEFT: TEXT (FULL WIDTH NOW) */}
+          {/* LEFT: TEXT */}
           <LeftColumn>
             <PageTitle>Privacy Policy</PageTitle>
 
@@ -132,6 +132,22 @@ export default function PrivacyPolicy() {
               </ul>
             </TextContent>
           </LeftColumn>
+
+          {/* RIGHT: SIDEBAR */}
+          <RightColumn>
+            <SidebarBox>
+              <PaymentImage>
+                <img src="/payment.png" alt="We Accept" />
+              </PaymentImage>
+              
+              <PhoneBtn phone="(800) 687-0480" />
+              <TextBubble />
+
+              <LogoImage>
+                 <img src="/logos/oscar-logo.png" alt="Oscars Lock & Key" />
+              </LogoImage>
+            </SidebarBox>
+          </RightColumn>
 
         </ContentRow>
       </MainWrapper>
@@ -288,6 +304,7 @@ const ContentRow = styled.div`
   max-width: 1300px;
   width: 100%;
   display: flex;
+  gap: 60px;
 
   ${media("<=largeDesktop")} {
     flex-direction: column;
@@ -295,8 +312,7 @@ const ContentRow = styled.div`
 `;
 
 const LeftColumn = styled.div`
-  flex: 1;
-  width: 100%;
+  flex: 3;
 `;
 
 const RightColumn = styled.div`
@@ -336,7 +352,7 @@ const LogoImage = styled.div`
 
 const PageTitle = styled.h1`
   font-family: "Times New Roman", serif;
-  font-size: 3rem;
+  font-size: 2rem;
   color: #0A3161;
   font-weight: 700;
   text-align: center;
@@ -344,13 +360,13 @@ const PageTitle = styled.h1`
   line-height: 1.1;
   
   ${media("<=tablet")} {
-    font-size: 2.4rem;
+    font-size: 1.6rem;
   }
 `;
 
 const PageSubTitle = styled.h2`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.8rem;
+  font-size: 1.4rem;
   color: #1e4d8b;
   font-weight: 700;
   text-align: center;
@@ -359,13 +375,13 @@ const PageSubTitle = styled.h2`
   line-height: 1.3;
   
   ${media("<=tablet")} {
-    font-size: 1.6rem;
+    font-size: 1.2rem;
   }
 `;
 
 const IntroText = styled.p`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.4rem;
+  font-size: 1.1rem;
   color: #1e4d8b;
   margin-bottom: 1rem;
   font-weight: 700;
@@ -374,7 +390,7 @@ const IntroText = styled.p`
 
 const TextContent = styled.div`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.6rem;
+  font-size: 1.1rem;
   line-height: 1.6;
   color: #1e4d8b;
 
@@ -409,13 +425,13 @@ const TextContent = styled.div`
   }
 
   ${media("<=tablet")} {
-    font-size: 1.4rem;
+    font-size: 0.9rem;
   }
 `;
 
 const SectionHeader = styled.h3`
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.8rem;
+  font-size: 1.3rem;
   color: #1e4d8b;
   font-weight: 700;
   margin-top: 1.5rem;
@@ -425,7 +441,7 @@ const SectionHeader = styled.h3`
   padding-bottom: 10px;
 
   ${media("<=tablet")} {
-    font-size: 1.6rem;
+    font-size: 1.1rem;
   }
 `;
 
